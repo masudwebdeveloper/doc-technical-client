2ce6e0bea5132777edb02abf4beba2b5ba8729d9
masudrana5 <abdullainfo30@gmail.com>
2022-10-26 13:07:04 +0600

    talented section is almost done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { FaBootstrap, FaCss3, FaHtml5, FaNodeJs, FaPlay, FaReact } from 'react-icons/fa';
+import { FaArrowRight, FaBootstrap, FaCss3, FaHtml5, FaNodeJs, FaPlay, FaReact } from 'react-icons/fa';
 import heroPhoto from '../../assets/brands/hero-photo.jpg'
 import photo1 from '../../assets/brands/masud-pic.jpg';
 import photo2 from '../../assets/brands/guides.png';
+import photo3 from '../../assets/brands/productive.png';
+import photo4 from '../../assets/brands/developer.png';
 
 const Home = () => {
 
    return (
       <div>
          <section className='dark:bg-gray-800 py-5 lg:py-10'>
-            <div className='grid grid-cols-1 lg:grid-cols-2 gap-5 container px-3 lg:px-20'>
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-5 container mx-auto  px-3'>
                <div className='flex flex-col justify-center p-5 lg:p-10'>
                   <h1 className='text-4xl lg:text-6xl mb-3 font-bold text-white'>I want work for NASA</h1>
                   <p className='text-lg lg:text-xl font-semibold text-white'>Online learning has become an integral component of higher education, as is evident by the steady emergence and growth of online course offerings and online academic and professional degree programs</p>
@@ -20,7 +22,7 @@ const Home = () => {
                </div>
             </div>
          </section >
-         <section className="light:bg-gray-800 py-5 lg:py-10 dark:text-gray-900 container lg:px-20">
+         <section className="light:bg-gray-800 py-5 lg:py-10 dark:text-gray-900 container mx-auto lg:px-20">
             <div className="container flex flex-col mx-auto lg:flex-row">
                <div className="w-full lg:w-1/3 p-5 relative" >
                   <img className='rounded-lg' src={photo1} alt="" />
@@ -90,26 +92,37 @@ const Home = () => {
             </div>
          </section>
 
-         <section className="light:bg-gray-800 dark:text-gray-800 lg:px-20">
+         <section className="light:bg-gray-800 dark:text-gray-800 lg:px-20 pb-5">
             <div className="container mx-auto flex flex-col items-center px-5 py-10 text-center  xl:max-w-3xl">
                <h1 className="text-4xl font-bold leading-none sm:text-5xl">Find best
                   <span className="dark:text-violet-400"> Talented</span> Candidate
                </h1>
-               <div className="flex flex-wrap justify-center">
-                  <button className="px-8 py-3 m-2 text-lg font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Get started</button>
+               <div className="flex flex-wrap flex-row justify-center hover:translate-x-3 mt-5 transition duration-500">
+                  <button className="px-8 py-3 m-2 text-lg font-semibold rounded dark:bg-violet-400 dark:text-gray-900"> Successful Student <FaArrowRight className='inline ml-2'></FaArrowRight> </button>
                </div>
             </div>
-            <div class="container mx-auto px-5 lg:px-10 p-10 grid grid-cols-4 grid-rows-2 grid-flow-col gap-4">
-               <div class="row-span-3 col-span-2 bg-gray-800 rounded-xl flex items-center">
-                  <img className='p-5' src={photo2} alt="" />
+            <div className="container mx-auto px-5 lg:p-10 grid grid-flow-col-1 lg:grid-cols-4 gap-4">
+               <div className="lg:row-span-4 col-span-4 lg:col-span-2 bg-gray-800 rounded-xl flex flex-col p-5 " data-aos="fade-left">
+                  <img className='w-48 mb-2' src={photo2} alt="" />
                   <div className='pr-3'>
                      <h3 className='text-white text-3xl font-semibold'>Candidate Guides</h3>
                      <p className='text-white'>We shortlist and pre-assess resumes and candidates based on your unique needs.</p>
                   </div>
-
                </div>
-               <div class="col-span-2">02</div>
-               <div class="col-span-2">03</div>
+               <div className="col-span-4 lg:col-span-2 row-span-2 flex items-center bg-gray-800 rounded-xl " data-aos="fade-up">
+               <img className='w-48 mb-2 p-4' src={photo3} alt="" />
+                  <div className='pr-3'>
+                     <h3 className='text-white text-3xl font-semibold'>Productive team</h3>
+                     <p className='text-white'>Build high performing teams crafted just for you with the right fit for your specific requirements.</p>
+                  </div>
+               </div>
+               <div className="col-span-4 lg:col-span-2 row-span-2 flex items-center bg-gray-800 rounded-xl" data-aos="fade-down">
+               <img className='w-48 mb-2 p-4' src={photo4} alt="" />
+                  <div className='pr-3'>
+                     <h3 className='text-white text-3xl font-semibold'>Futuristic Developer</h3>
+                     <p className='text-white'>Hire energetic Developers who are challenge takers and a quick learner.</p>
+                  </div>
+               </div>
             </div>
          </section>
       </div>
